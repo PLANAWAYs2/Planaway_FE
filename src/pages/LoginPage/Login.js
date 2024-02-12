a1e9879f61b0d257245a7a3d1f5ee528b6f1d789
YoungseoChoi23 <dudtj9326@ewha.ac.kr>
2024-02-13 05:35:23 +0900

    feat : login 값이 입력되지 않았을 경우 focus 기능 추가

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,5 +1,33 @@
 import './Login.css';
+import { useState, useRef } from 'react';
 const Login = () => {
+  const idInput = useRef();
+  const passwordInput = useRef();
+
+  const [state, setState] = useState({
+    id: '',
+    password: '',
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    if (state.id.length < 1) {
+      idInput.current.focus();
+      return;
+    }
+
+    if (state.password.length < 1) {
+      passwordInput.current.focus();
+      return;
+    }
+  };
+
   return (
     <div className="LoginPage">
       <div className="Login">로그인</div>
@@ -8,14 +36,26 @@ const Login = () => {
           <div className="idPw">
             <div className="id">
               <span> 아이디 </span>
-              <input />
+              <input
+                ref={idInput}
+                name="id"
+                value={state.id}
+                onChange={handleChangeState}
+              />
             </div>
             <div className="password">
               <span> 비밀번호</span>
-              <input />
+              <input
+                ref={passwordInput}
+                name="password"
+                value={state.content}
+                onChange={handleChangeState}
+              />
             </div>
           </div>
-          <button className="LoginButton">로그인</button>
+          <button onClick={handleSubmit} className="LoginButton">
+            로그인
+          </button>
         </div>
         <div className="add">
           <div className="Signup">
