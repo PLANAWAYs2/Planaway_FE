c48270986da51cf2df95b0b7dcdb987d492a60f2
mmaybei <yebbi2@ewhain.net>
2024-02-19 15:34:34 +0900

    fix: 지도 크기 수정

--- src/pages/PlanSetsPage/PlanSets.js
+++ src/pages/PlanSetsPage/PlanSets.js
@@ -1,11 +1,11 @@
 import { Container } from "../../Layout";
 import Header from "../../components/Header";
 import MyPageSideBar from "../../components/MyPageSideBar";
-import PlanSideBar from "../../components/PlanSideBar";
 import Profile from "../../components/Profile";
 import "./PlanSets.css";
 
 const PlanSets = () => {
+  const API_KEY = "AIzaSyDedTPh8y0kQKaWqEGKnPwXAMXhLeENOHU";
   return (
     <>
       <Header />
@@ -16,6 +16,14 @@ const PlanSets = () => {
           </div>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <Profile />
+            <div>
+              <iframe
+                width="828px"
+                height="448px"
+                frameborder="0"
+                src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=프랑스&zoom=6`}
+              />
+            </div>
           </div>
         </div>
       </Container>
