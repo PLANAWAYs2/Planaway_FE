7f1677d0f315816b4810094242abcbf4493d1e38
mmaybei <yebbi2@ewhain.net>
2024-02-19 15:11:19 +0900

    feat: plansets 페이지 레이아웃 작성

--- src/pages/PlanSetsPage/PlanSets.js
+++ src/pages/PlanSetsPage/PlanSets.js
@@ -1,3 +1,26 @@
-const PlanSets = () => {};
+import { Container } from "../../Layout";
+import Header from "../../components/Header";
+import MyPageSideBar from "../../components/MyPageSideBar";
+import PlanSideBar from "../../components/PlanSideBar";
+import Profile from "../../components/Profile";
+import "./PlanSets.css";
+
+const PlanSets = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <div style={{ display: "flex" /*justifyContent: "space-between"*/ }}>
+          <div className="MPframe">
+            <MyPageSideBar />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Profile />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
 export default PlanSets;
