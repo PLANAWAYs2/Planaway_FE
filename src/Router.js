efd26e9f922b7454c28c8bc6988fcb46ad815624
mmaybei <yebbi2@ewhain.net>
2024-02-22 03:36:34 +0900

    feat: 계획업로드4 페이지

--- src/Router.js
+++ src/Router.js
@@ -7,6 +7,7 @@ import PlanList from "./pages/PlanListPage/PlanList";
 import PlanSets from "./pages/PlanSetsPage/PlanSets";
 import SignUp from "./pages/SignUpPage/SignUp";
 import UploadPlan3 from "./pages/UploadPlanPage/UploadPlan3";
+import UploadPlan4 from "./pages/UploadPlanPage/UploadPlan4";
 import UserInfo from "./pages/UserInfoPage/UserInfo";
 import ViewPlan from "./pages/ViewPlanPage/ViewPlan";
 import WishList from "./pages/WishListPage/WishList";
@@ -22,6 +23,7 @@ const Router = () => {
         <Route path="/plansets" element={<PlanSets />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/uploadplan3" element={<UploadPlan3 />} />
+        <Route path="/uploadplan4" element={<UploadPlan4 />} />
         <Route path="/userinfo" element={<UserInfo />} />
         <Route path="/viewplan" element={<ViewPlan />} />
         <Route path="/wishlist" element={<WishList />} />
---
+++ src/pages/UploadPlanPage/PlanEditor4.js
@@ -0,0 +1,93 @@
+import { useRef, useState } from "react";
+import {
+  PE3Wrapper,
+  PE3Input,
+  PEContents,
+  PE3ContentTitle,
+  PEMap,
+  PEMapWrapper,
+  PE3ContentBox,
+  PE3ContentBoxLine,
+  PE3Container,
+} from "./PlanEditor3Style";
+import PE3InputContainer from "../../components/PE3InputContainer";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+
+const PlanEditor4 = () => {
+  const titleRef = useRef();
+  const [title, setTitle] = useState();
+
+  return (
+    <PE3Wrapper>
+      <PE3Input
+        placeholder="제목을 입력하세요."
+        value={title}
+        ref={titleRef}
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+      />
+      <hr />
+      <PE3Container>
+        <PE3ContentBoxLine>
+          <PE3ContentBox>
+            <PE3ContentTitle>
+              <h4>프랑스</h4>
+            </PE3ContentTitle>
+
+            <PEContents>
+              <PEMapWrapper>
+                <PE3InputContainer />
+              </PEMapWrapper>
+            </PEContents>
+          </PE3ContentBox>
+        </PE3ContentBoxLine>
+
+        <PE3ContentBoxLine>
+          <PE3ContentBox>
+            <PE3ContentTitle>
+              <h4>이탈리아</h4>
+            </PE3ContentTitle>
+
+            <PEContents>
+              <PEMapWrapper>
+                <PE3InputContainer />
+              </PEMapWrapper>
+            </PEContents>
+          </PE3ContentBox>
+        </PE3ContentBoxLine>
+
+        <PE3ContentBoxLine>
+          <PE3ContentBox>
+            <PE3ContentTitle>
+              <h4>독일</h4>
+            </PE3ContentTitle>
+
+            <PEContents>
+              <PEMapWrapper>
+                <PE3InputContainer />
+              </PEMapWrapper>
+            </PEContents>
+          </PE3ContentBox>
+        </PE3ContentBoxLine>
+
+        <PE3ContentBoxLine>
+          <PE3ContentBox>
+            <PE3ContentTitle>
+              <h4>면세점</h4>
+            </PE3ContentTitle>
+
+            <PEContents>
+              <PEMapWrapper>
+                <PE3InputContainer />
+              </PEMapWrapper>
+            </PEContents>
+          </PE3ContentBox>
+        </PE3ContentBoxLine>
+      </PE3Container>
+    </PE3Wrapper>
+  );
+};
+
+export default PlanEditor4;
