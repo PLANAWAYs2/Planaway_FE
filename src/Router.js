6ab3de574f3e121cf4a48cf069a2c06e8984be2c
mmaybei <yebbi2@ewhain.net>
2024-02-29 04:04:36 +0900

    fix: router, headr 연결 수정

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/MainPage/Main';
-import Login from './pages/LoginPage/Login';
-import PlanDetail from './pages/PlanDetailPage/PlanDetail';
-import PlanList from './pages/PlanListPage/PlanList';
-import PlanSets from './pages/PlanSetsPage/PlanSets';
-import SignUp from './pages/SignUpPage/SignUp';
-import UploadPlan2 from './pages/UploadPlanPage/UploadPlan2';
-import UserInfo from './pages/UserInfoPage/UserInfo';
-import ViewPlan from './pages/ViewPlanPage/ViewPlan';
-import WishList from './pages/WishListPage/WishList';
-import SearchPage from './pages/SearchPage/SearchPage';
-import FindId from './pages/FindInfoPage/FindId';
-import FindPw from './pages/FindInfoPage/FindPw';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/MainPage/Main";
+import Login from "./pages/LoginPage/Login";
+import PlanDetail from "./pages/PlanDetailPage/PlanDetail";
+import PlanList from "./pages/PlanListPage/PlanList";
+import PlanSets from "./pages/PlanSetsPage/PlanSets";
+import SignUp from "./pages/SignUpPage/SignUp";
+import UploadPlan1 from "./pages/UploadPlanPage/UploadPlan1";
+import UploadPlan2 from "./pages/UploadPlanPage/UploadPlan2";
+import UploadPlan3 from "./pages/UploadPlanPage/UploadPlan3";
+import UploadPlan4 from "./pages/UploadPlanPage/UploadPlan4";
+import UserInfo from "./pages/UserInfoPage/UserInfo";
+import ViewPlan from "./pages/ViewPlanPage/ViewPlan";
+import WishList from "./pages/WishListPage/WishList";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import FindId from "./pages/FindInfoPage/FindId";
+import FindPw from "./pages/FindInfoPage/FindPw";
 
 export const SearchKeyword = React.createContext();
 export const DispatchContext = React.createContext();
@@ -33,7 +36,10 @@ const Router = () => {
             <Route path="/planlist" element={<PlanList />} />
             <Route path="/plansets" element={<PlanSets />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/uploadplan" element={<UploadPlan2 />} />
+            <Route path="/uploadplan1" element={<UploadPlan1 />} />
+            <Route path="/uploadplan2" element={<UploadPlan2 />} />
+            <Route path="/uploadplan3" element={<UploadPlan3 />} />
+            <Route path="/uploadplan4" element={<UploadPlan4 />} />
             <Route path="/userinfo" element={<UserInfo />} />
             <Route path="/viewplan" element={<ViewPlan />} />
             <Route path="/wishlist" element={<WishList />} />
